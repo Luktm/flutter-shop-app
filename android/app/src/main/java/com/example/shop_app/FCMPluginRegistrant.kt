259dbd613f18d8f9c2421d12f92afae622019b20
Luk Tze Ming <luk_ltm@hotmail.com>
2020-10-05 14:47:50 +0800

    Add files via upload

---
+++ android/app/src/main/java/com/example/shop_app/FCMPluginRegistrant.kt
@@ -0,0 +1,25 @@
+package com.dropd.shopper
+
+import io.flutter.plugin.common.PluginRegistry
+import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+
+
+object FirebaseCloudMessagingPluginRegistrant {
+    fun registerWith(registry: PluginRegistry) {
+        if (alreadyRegisteredWith(registry)) {
+            return
+        }
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
+        FlutterLocalNotificationsPlugin.registerWith(registry.registrarFor("com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"))
+    }
+
+    private fun alreadyRegisteredWith(registry: PluginRegistry): Boolean {
+        val key = FirebaseCloudMessagingPluginRegistrant::class.java.canonicalName
+        if (registry.hasPlugin(key)) {
+            return true
+        }
+        registry.registrarFor(key)
+        return false
+    }
+}
